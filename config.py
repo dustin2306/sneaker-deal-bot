520fa4b73a9abbff1db893acde46ce29fb6b9f20
dustin2306 <dasberg1707@gmail.com>
2026-01-29 22:50:45 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+MIN_RABATT = 70
+SOLEBOX_MIN_RABATT = 60
+
+MARKEN = [
+    "nike",
+    "adidas",
+    "reebok",
+    "converse",
+    "new balance",
+    "asics"
+]
+
+GROESSEN = list(range(36, 49))
+SCAN_INTERVAL = 3600
+PING_TEXT = "@everyone
